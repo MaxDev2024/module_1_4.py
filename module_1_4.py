e79af03112cedb95c2641a37400bf86ab84818d7
MaxDev2024 <maksimkalacky2009@gmail.com>
2024-10-23 15:04:22 +0900

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Укажите регион Вашего проживания (Например: "Московская область"):')
+print('Название Вашего региона состоит из',len(my_string),'символов')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace('',''))
+print(my_string[0])
+print(my_string[-1])
