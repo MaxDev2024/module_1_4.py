d7a554da6231fdadd2dd8ac41e276de42ccb4776
MaxDev2024 <maksimkalacky2009@gmail.com>
2024-10-23 15:17:27 +0900

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -2,6 +2,6 @@ my_string = input('Укажите регион Вашего проживания
 print('Название Вашего региона состоит из',len(my_string),'символов')
 print(my_string.upper())
 print(my_string.lower())
-print(my_string.replace('',''))
+print(my_string.replace(' ',''))
 print(my_string[0])
 print(my_string[-1])
